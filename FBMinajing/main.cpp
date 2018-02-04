fea7b7a669b464753f2751db7f2f68f39f95f23d
garyx <xushuyu1987@gmail.com>
2018-02-03 16:23:26 -0800

    Initial Commit

---
+++ FBMinajing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FBMinajing
+//
+//  Created by Shuyu on 2/3/18.
+//  Copyright © 2018 Shuyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
